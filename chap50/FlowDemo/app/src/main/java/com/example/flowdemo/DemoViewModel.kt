1cb3b652e469be111bb8be68c32ed3d0d80dbce8
crane <dg.yang@last30min.com>
2023-03-13 18:59:26 +0900

    chap 50 complete

--- chap50/FlowDemo/app/src/main/java/com/example/flowdemo/DemoViewModel.kt
+++ chap50/FlowDemo/app/src/main/java/com/example/flowdemo/DemoViewModel.kt
@@ -2,17 +2,53 @@ package com.example.flowdemo
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.compose.viewModel
+import kotlinx.coroutines.channels.BufferOverflow
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
 
 class DemoViewModel : ViewModel() {
-    val myFlow: Flow<Int> = flow {
-        for(i in 1..9){
-            emit(i)
-            delay(2000)
+    private val _sharedFlow = MutableSharedFlow<Int>(
+        replay = 10,
+        onBufferOverflow = BufferOverflow.DROP_OLDEST
+    )
+
+    val sharedFlow = _sharedFlow.asSharedFlow()
+    fun startSharedFlow(){
+        viewModelScope.launch {
+            for(i in 1..5){
+                _sharedFlow.emit(i)
+                delay(2000)
+            }
         }
+    }
+    val subCount = _sharedFlow.subscriptionCount
+
+    private val _stateFlow = MutableStateFlow(0)
+    val stateFlow = _stateFlow.asStateFlow()
+
+    fun increaseValue(){
+        _stateFlow.value += 1
+    }
+
+    val myFlow: Flow<Int> = flow {
+        for(i in 1..5){
+            delay(1000)
+            emit(i)
+            Log.e("Crane", "myFlow : $i")
+        }
+    }
+
+    val hotFlow = myFlow.shareIn(viewModelScope, replay = 1, started = SharingStarted.WhileSubscribed())
+
+    fun doubleIt(value: Int) = flow {
+        emit(value)
+        Log.e("Crane", "doubleIt x : $value")
+        delay(1000)
+        emit(value + value)
+        Log.e("Crane", "doubleIt x x x : $value + value")
     }
 
     val newFlow = myFlow.map {
--- chap50/FlowDemo/app/src/main/java/com/example/flowdemo/MainActivity.kt
+++ chap50/FlowDemo/app/src/main/java/com/example/flowdemo/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -24,14 +25,9 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import com.example.flowdemo.ui.theme.FlowDemoTheme
 import androidx.lifecycle.viewmodel.compose.viewModel
+import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.buffer
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.flow.conflate
-import kotlinx.coroutines.flow.fold
-import kotlinx.coroutines.flow.reduce
+import kotlinx.coroutines.flow.*
 import kotlin.system.measureTimeMillis
 
 class MainActivity : ComponentActivity() {
@@ -48,15 +44,89 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun ScreenSetup(viewModel: DemoViewModel = viewModel()) {
+fun ScreenSetup(viewModel : DemoViewModel = viewModel()) {
     // MainScreen(viewModel.newFlow)
-    MainScreen2(viewModel.myFlow)
+    MainScreen6(viewModel)
+}
+
+@Composable
+fun MainScreen6(viewModel : DemoViewModel) {
+    val count by viewModel.sharedFlow.collectAsState(initial = 0)
+    val count2 by viewModel.sharedFlow.collectAsState(initial = 0)
+    val count3 by viewModel.sharedFlow.collectAsState(initial = 0)
+    val num by viewModel.subCount.collectAsState(initial = 0)
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "$count $num", style = TextStyle(fontSize = 40.sp))
+        Button(onClick = { viewModel.startSharedFlow()}){
+            Text("Click Me")
+        }
+    }
+}
+
+
+
+@Composable
+fun MainScreen5(viewModel : DemoViewModel) {
+    val count by viewModel.stateFlow.collectAsState()
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "$count", style = TextStyle(fontSize = 40.sp))
+        Button(onClick = { viewModel.increaseValue()}){
+            Text("Click Me")
+        }
+    }
+}
+
+@Composable
+fun MainScreen4(viewModel : DemoViewModel) {
+    var count by remember { mutableStateOf("") }
+    LaunchedEffect(key1 = Unit) {
+        val flow1 = (1 .. 5).asFlow().onEach { delay(1000) }
+        val flow2 = flowOf("one", "two", "three", "four").onEach { delay(1500) }
+        flow1.combine(flow2) { value, string -> "$value, $string" }
+            .collect { count = it }
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "$count", style = TextStyle(fontSize = 40.sp))
+    }
+}
+
+@OptIn(FlowPreview::class)
+@Composable
+fun MainScreen3(viewModel : DemoViewModel) {
+    var count by remember { mutableStateOf(0) }
+
+    LaunchedEffect(key1 = Unit) {
+        viewModel.myFlow.flatMapMerge { viewModel.doubleIt(it) }
+            .collect { count = it }
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "$count", style = TextStyle(fontSize = 40.sp))
+    }
 }
 
 @Composable
 fun MainScreen2(flow : Flow<Int>) {
-    var count by remember { mutableStateOf(0)}
-    LaunchedEffect(key1 = Unit){
+    var count by remember { mutableStateOf(0) }
+    LaunchedEffect(key1 = Unit) {
         flow.fold(10) { accumulator, value ->
             count = accumulator + value
             Log.e("Crane", "Proc >> idx : value : $value / acc : $accumulator / count : $count result : ${accumulator + value}")
@@ -74,8 +144,8 @@ fun MainScreen(flow : Flow<String>) {
     //     Log.e("Crane", "Proc >> idx : $count")
     // }
 
-    var count by remember { mutableStateOf("Current value=")}
-    LaunchedEffect(key1 = Unit){
+    var count by remember { mutableStateOf("Current value=") }
+    LaunchedEffect(key1 = Unit) {
         val elapsedTime = measureTimeMillis {
             flow.buffer().collect {
                 count = it
